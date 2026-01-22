585ec735f1075cb325bfcf14504c93e8a67a4957
Arthur <cotikgg@gmail.com>
2026-01-22 10:35:19 +0100

    feat: add burger menu

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Meow');
+// querySelector
+const burgerMenu = document.querySelector('.nav__burger');
+const burgerBtn = document.querySelector('.nav__button');
+
+// functions
+const openBurger = () => {
+    burgerMenu.classList.add('burger__menu--active');
+};
+
+const closeBurger = () => {
+    burgerMenu.classList.remove('burger__menu--active')
+};
